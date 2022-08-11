4edc654abf7235d94b1a70af204e575abd1cc401
ankit-deshmukh <ankit.deshmukh@smallcase.com>
2022-08-11 17:15:48 +0530

    build: removed set header method

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ var path = require('path');
 
 app.use(express.static(path.join(__dirname)));
 
-// viewed at based directory http://localhost:8080/
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.get('/.well-known/apple-app-site-association', function (req, res) {
+
+  console.log("sending apple app site association file ...");
   res.header('Content-Type', 'application/json');
   // res.setHeader('Content-Type', 'application/json');
   res.sendFile(path.join(__dirname + '/.well-known/apple-app-site-association'));
